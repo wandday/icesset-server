3d3d88b58679005dc857de4ebe4ac6b6343ab5f1
wandday <wande_fatodu@yahoo.com"git config --global user.name "wandday"git config --global user.email wande_fatodu@yahoo.comgit config --global user.name wanddaygit config --global user.email 'wande_fatodu@yahoo.com>
2022-08-25 17:18:19 +0100

    fix transaction status issue

--- src/models/transaction.js
+++ src/models/transaction.js
@@ -3,14 +3,14 @@ import {pool } from '../index'
 
 export const createTransaction = async (trans) => {
     // console.log(trans)
-    const { waybillDetails: { destination, sent_to_id, sent_to_name, courier_name, courier_contact, note}, transactionDetails:{transaction_type, created_by_id, created_by_name, receivedBy, stored_in}, transactionItem} = trans
+    const { waybillDetails: { destination, sent_to_id, sent_to_name, courier_name, courier_contact, note}, transactionDetails:{transaction_type, created_by_id, created_by_name, stored_in}, transactionItem} = trans
     let waybillDetails = await pool.query('INSERT into waybill SET destination=?, sent_to_id=?, sent_to_name=?, courier_name=?, courier_contact=?, note=?',  [destination, sent_to_id, sent_to_name, courier_name, courier_contact, note ])
     
     let result = await pool.query('SELECT LAST_INSERT_ID()');
     let waybillId =  result[0][0][`LAST_INSERT_ID()`]
     console.log(waybillId)
 
-    let transactionDetails = await pool.query('INSERT into transactions SET transaction_status=?, transaction_type=?, waybill_id=?, created_by_id=?, created_by_name=?, receivedBy=?, stored_in=?' ,  ['Pending', transaction_type, waybillId, created_by_id, created_by_name, receivedBy, stored_in ])
+    let transactionDetails = await pool.query('INSERT into transactions SET transaction_status=?, transaction_type=?, waybill_id=?, created_by_id=?, created_by_name=?, receivedBy=?, stored_in=?' ,  ['Pending', transaction_type, waybillId, created_by_id, created_by_name, 'Pending', stored_in ])
 
     let response = await pool.query('SELECT LAST_INSERT_ID()');
     let transId =  response[0][0][`LAST_INSERT_ID()`]

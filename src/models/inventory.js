b1e96092f55b99a3087bed970063bee22a192230
wandday <wande_fatodu@yahoo.com"git config --global user.name "wandday"git config --global user.email wande_fatodu@yahoo.comgit config --global user.name wanddaygit config --global user.email 'wande_fatodu@yahoo.com>
2022-08-26 12:30:04 +0100

    sort inventory by last added

--- src/models/inventory.js
+++ src/models/inventory.js
@@ -33,7 +33,7 @@ export const getItemsWithPerson = async (userId) => {
 }
 
 export const findAllItem = async () => {
-   return await pool.query('select * from items INNER JOIN quantity_location ON items.item_id = quantity_location.item_id')
+   return await pool.query('select * from items INNER JOIN quantity_location ON items.item_id = quantity_location.item_id ORDER BY quantity_location.qyt_loc_id DESC')
 }
 
 export const findItem = async (keyWord) => {

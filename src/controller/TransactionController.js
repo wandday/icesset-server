a6c871edda73e57111a99bdd739999c9ae359fda
wandday <wande_fatodu@yahoo.com>
2022-09-21 15:12:03 +0100

    set cron job to 5mins

--- src/controller/TransactionController.js
+++ src/controller/TransactionController.js
@@ -248,8 +248,8 @@ export default class TransactionController {
 }
 // Reminder Email For Sent Items -CRON JOB
 
- let task = cron.schedule('*/2 * * * *', async () => {
-    console.log('running a task every twelve hours');
+ let task = cron.schedule('*/5 * * * *', async () => {
+    console.log('running a task every two mins');
 
     let currentDate = new Date();
         let result = await checkDeliveryDate()
